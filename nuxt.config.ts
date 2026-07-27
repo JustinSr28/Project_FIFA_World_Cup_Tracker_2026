b7398cbf46c99e9bc15ba9be56df69a94739d500
JustinSr28 <sanchoruizjustin@gmail.com>
2026-07-27 13:44:49 -0600

    Setup inicial: Nuxt 4 + Pinia + Firebase (Auth y Firestore)

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint','@pinia/nuxt'],
+  runtimeConfig: {
+    public: {
+      firebaseApiKey: '',
+      firebaseAuthDomain: '',
+      firebaseProjectId: '',
+      firebaseStorageBucket: '',
+      firebaseMessagingSenderId: '',
+      firebaseAppId: ''
+    }
+  }
+})
+
